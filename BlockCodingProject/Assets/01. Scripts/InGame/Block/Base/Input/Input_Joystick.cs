ddf1f77d6d67c84d6913bfe750530e66f2db7be8
Double_U_ <junhyup511@naver.com>
2021-12-06 00:37:32 +0900

    파티클 메타 수정, 조이스틱 코드 수정

--- BlockCodingProject/Assets/01. Scripts/InGame/Block/Base/Input/Input_Joystick.cs
+++ BlockCodingProject/Assets/01. Scripts/InGame/Block/Base/Input/Input_Joystick.cs
@@ -4,6 +4,44 @@ using UnityEngine;
 
 public class Input_Joystick : BaseBlockScript
 {
+    public override void OnConnected(BlockScript connectedBy)
+    {
+        base.OnConnected(connectedBy);
+        ChildBlockScript child = connectedBy.GetComponent<ChildBlockScript>();
+
+        if (child != null)
+        {
+            ObjectCollider coll = child.inGameObj.GetComponent<ObjectCollider>();
+            if (coll != null)
+            {
+                coll.SetCollision();
+                for (int i = 0; i < coll.collisionList.Count; i++)
+                {
+                    ObjectCollider item_coll = coll.collisionList[i].GetComponent<ObjectCollider>();
+                    if (item_coll != null)
+                    {
+                        item_coll.ActionPlay();
+                    }
+                }
+            }
+        }
+    }
+
+    public override void OnDisconnected(BlockScript disconnectedBy)
+    {
+        base.OnDisconnected(disconnectedBy);
+        ChildBlockScript child = disconnectedBy.GetComponent<ChildBlockScript>();
+
+        if (child != null)
+        {
+            ObjectCollider coll = child.inGameObj.GetComponent<ObjectCollider>();
+            if (coll != null)
+            {
+                coll.SetTrigger();
+            }
+        }
+    }
+
     public override void BlockAbility(GameObject obj)
     {
         Vector2 joyStickDir = new Vector2(JoyStickInput.horizontalRaw, JoyStickInput.verticalRaw);
